49d1bcac0e0b4b4ccf9ce88982d80eacdb764b18
joffre <joffre.hermosilla@gmail.com>
2024-03-22 18:26:31 -0500

    chore[2] segunda prueba con main

---
+++ src/main/java/coderpath/quarkus/ExampleResource.java
@@ -0,0 +1,33 @@
+package coderpath.quarkus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class ExampleResource {
+
+@ConfigProperty(name="greeting")
+    private String greeting;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("custom/{name}")
+    public String customHello(@PathParam("name") String name){
+        return greeting + name + " how are you doing";
+    }
+
+
+
+
+
+}
---
+++ src/test/java/coderpath/quarkus/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package coderpath.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
