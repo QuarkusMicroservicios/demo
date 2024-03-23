1b836d33f383e08d768723e8a091eb7d3b52b1ce
joffre <joffre.hermosilla@gmail.com>
2024-03-22 23:19:00 -0500

    chore[4] configuracion de inicio y cierre de app en quarkus

---
+++ src/main/java/coderpath/quarkus/AppLifecycleBean.java
@@ -0,0 +1,25 @@
+package coderpath.quarkus;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+
+    private static final    Logger LOGGER  = Logger.getLogger("ListnerBean");
+
+    void onStart(@Observes StartupEvent evt) {
+        LOGGER.info("The application is starting ...");
+    }
+
+    void onStop(@Observes ShutdownEvent evt){
+
+        LOGGER.info("The application is stopping ...");
+    }
+
+
+}
